5bf270814c40b7ddc32a1a1b9f9a457ce60e2cab
phamvanmien1111 <phamvanmien2001@gmail.com>
2025-03-01 01:45:23 +0700

    Cập nhật README.md

--- src/middlewares/checkRole.ts
+++ src/middlewares/checkRole.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 const checkRole = (roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction): void => {
-    const user = (req as any).user; // Nếu `req.user` không tồn tại, cần ép kiểu
+    const user = (req as any).user; 
 
     if (!user || !roles.includes(user.role)) {
       res.status(403).json({ message: "Forbidden" });
